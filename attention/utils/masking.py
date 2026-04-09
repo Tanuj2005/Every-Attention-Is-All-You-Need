e9b64c0c25160e57e58b7d96d5a1a1c280d43c76
ORBEX <tanujtyagi2005@gmail.com>
2026-04-10 01:13:19 +0530

    changed the structure

---
+++ attention/utils/masking.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+ 
+import torch
+from torch import Tensor
+ 
+__all__ = [
+    "causal_mask",
+    "padding_mask_to_attn_bias",
+    "combine_masks",
+    "make_local_window_mask",
+    "make_block_diagonal_mask",
+    "NEG_INF",
+]
+
+
+def causal_mask():
+    pass
+
+def padding_mask_to_attn_bias():
+    pass
+
+def combine_masks():
+    pass
+
+def make_local_window_mask():
+    pass
+
+def make_block_diagonal_mask():
+    pass
+
+
+
+
